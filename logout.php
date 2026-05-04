eebfc6a596458e324c7228c5ecf8a3343e1008ec
Ranjini-ctrl <shivasakthi2214@gmail.com>
2026-05-04 20:26:48 +0530

    commit

---
+++ logout.php
@@ -0,0 +1,20 @@
+<?php
+session_start();
+include "../config/db.php"; // adjust path if needed
+
+// Check if session exists
+if (isset($_SESSION['user'])) {
+    $email = $_SESSION['user'];
+
+    // Update user status to offline
+    mysqli_query($conn, "UPDATE users SET status='offline' WHERE email='$email'");
+}
+
+// Destroy session
+$_SESSION = [];
+session_destroy();
+
+// Redirect to login page
+header("Location:index.html");
+exit();
+?>
